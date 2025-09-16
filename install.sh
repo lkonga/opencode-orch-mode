79ace42447d2ac20afa6182ff72f761d9bfdc948
aptdnfapt <197602950+aptdnfapt@users.noreply.github.com>
2025-09-16 19:09:22 +0000

    rename old names

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 # Install issue-plan-sagent command to global opencode config
 mkdir -p ~/.config/opencode/command
 cp -r command/orch.md ~/.config/opencode/command/
-echo "Issue-plan-sagent command installed to ~/.config/opencode/command/"
+echo "ORCH command installed to ~/.config/opencode/command/"
